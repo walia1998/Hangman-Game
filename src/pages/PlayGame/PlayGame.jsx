908113537a69b89c1cd94b0c57b964be004a9daa
Ashish Walia <waliaashu1998@gmail.com>
2025-01-18 22:09:51 +0530

    Added Logic of buttons and selectedLetter

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import MaskedText from "../../components/MaskedText/MaskedText";
 import LetterButtons from "../../components/LetterButton/LetterButton";
 import { useState } from "react";
@@ -6,20 +6,24 @@ import { useState } from "react";
 function PlayGame() {
     
     const [usedLetters, setUsedLetters] = useState([]);
+    
+    const location = useLocation();
+    const wordSelected = location.state?.wordSelected; //if the state is not defined, it will return undefinded, Otherwise it'll retrun the value of wordselected.
+    
 
     const handleLetterClick = function(letter) {
         setUsedLetters([...usedLetters, letter])
     }
-    const text = "HUMBLE";
+    //const text = "HUMBLE";
     //const usedLetters = ['B', 'E']
 
     return (
 
         <div>
             <h1> Play Game</h1>
-            <MaskedText text={text} usedLetters={usedLetters} />
+            <MaskedText text={wordSelected} usedLetters={usedLetters} />
             <hr />
-            <LetterButtons text={text} usedLetters={usedLetters} onLetterClick={handleLetterClick}/>
+            <LetterButtons text={wordSelected} usedLetters={usedLetters} onLetterClick={handleLetterClick}/>
 
 
             <hr />
